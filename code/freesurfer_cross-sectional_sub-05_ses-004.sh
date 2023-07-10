2113b844d6d5181f46f9d0e5e7aba2d2602f7965
bpinsard <basile.pinsard@gmail.com>
2023-07-10 15:23:46 -0400

    [DATALAD] Recorded changes

--- code/freesurfer_cross-sectional_sub-05_ses-004.sh
+++ code/freesurfer_cross-sectional_sub-05_ses-004.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --mail-user=None
+#SBATCH --mail-user=basile.pinsard@gmail.com
 
  
 set -e -u -x
@@ -25,7 +25,7 @@ git checkout -b $SLURM_JOB_NAME
 
 git submodule foreach  --recursive git-annex enableremote ria-beluga-storage
 
-datalad containers-run -m 'fMRIPrep_sub-05/ses-004' -n containers/bids-freesurfer--6.0.1-6.1 --output . --input 'sourcedata/cneuromod.anat/sub-05/ses-004/anat/*_T1w.nii.gz' --input 'sourcedata/cneuromod.anat/sub-05/ses-004/anat/*_T2w.nii.gz' --input 'sourcedata/cneuromod.anat/sub-05/ses-004/anat/*_FLAIR.nii.gz' -- -w ./workdir --participant-label 05  --n_cpus 8 --fs-license-file code/freesurfer.license sourcedata/cneuromod.anat ./ participant 
+datalad containers-run -m 'fMRIPrep_sub-05/ses-004' -n containers/bids-freesurfer --output . --input 'sourcedata/cneuromod.anat/sub-05/ses-004/anat/*_T1w.nii.gz' --input 'sourcedata/cneuromod.anat/sub-05/ses-004/anat/*_T2w.nii.gz' --input 'sourcedata/cneuromod.anat/sub-05/ses-004/anat/*_FLAIR.nii.gz' -- -w ./workdir --participant-label 05  --n_cpus 8 --license-file code/freesurfer.license sourcedata/cneuromod.anat ./ participant 
 freesurfer_exitcode=$?
 
 flock --verbose /lustre03/project/rrg-pbellec/ria-beluga/alias/cneuromod.anat.freesurfer_longitudinal/.datalad_lock datalad push -d ./ --to origin
