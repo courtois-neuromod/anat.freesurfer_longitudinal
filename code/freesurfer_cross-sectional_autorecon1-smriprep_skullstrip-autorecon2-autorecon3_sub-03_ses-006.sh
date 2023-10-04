33472408b48f2d0170bf750deff0faf74ab3c82b
bpinsard <basile.pinsard@gmail.com>
2023-10-04 10:20:48 -0400

    [DATALAD] Recorded changes

---
+++ code/freesurfer_cross-sectional_autorecon1-smriprep_skullstrip-autorecon2-autorecon3_sub-03_ses-006.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --account=rrg-pbellec
+#SBATCH --job-name=freesurfer_cross-sectional_autorecon1-smriprep_skullstrip-autorecon2-autorecon3_sub-03_ses-006.job
+#SBATCH --output=./code/freesurfer_cross-sectional_autorecon1-smriprep_skullstrip-autorecon2-autorecon3_sub-03_ses-006.out
+#SBATCH --error=./code/freesurfer_cross-sectional_autorecon1-smriprep_skullstrip-autorecon2-autorecon3_sub-03_ses-006.err
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=4096M
+#SBATCH --tmp=100G
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=basile.pinsard@gmail.com
+
+
+set -e -u -x
+
+
+export LOCAL_DATASET=$SLURM_TMPDIR/${SLURM_JOB_NAME//-/}/
+flock --verbose /lustre03/project/rrg-pbellec/ria-beluga/alias/cneuromod.anat.freesurfer_longitudinal/.datalad_lock datalad clone ria+file:///lustre03/project/rrg-pbellec/ria-beluga#~cneuromod.anat.freesurfer_longitudinal@dev $LOCAL_DATASET
+cd $LOCAL_DATASET
+datalad get -s ria-beluga-storage -J 4 -n -r -R1 . # get sourcedata/* containers
+git annex dead here
+git checkout -b $SLURM_JOB_NAME
+
+git submodule foreach  --recursive bash -c "git-annex enableremote ria-beluga-storage || true"
+
+datalad containers-run -m 'freesurfer_sub-03/ses-006' -n bids-freesurfer --output . --input 'sourcedata/cneuromod.anat.gradcorrect/sub-03/ses-006/anat/*_T1w.nii.gz' --input 'sourcedata/cneuromod.anat.gradcorrect/sub-03/ses-006/anat/*_T2w.nii.gz'  -- sourcedata/cneuromod.anat.gradcorrect ./ participant --steps cross-sectional --stages autorecon1  --hires_mode enable --participant_label 03 --session_label 006 --skip_bids_validator --license_file code/freesurfer.license --n_cpus 4 
+datalad remove sub-03_ses-006/mri/brainmask.*datalad containers-run -n brain_xtract $PWD sub-03_ses-006 sourcedata/cneuromod.anat.gradcorrect/sub-03/ses-006/anat/*_T1w.nii.gzdatalad containers-run -m 'freesurfer_sub-03/ses-006' -n bids-freesurfer --output . --input 'sourcedata/cneuromod.anat.gradcorrect/sub-03/ses-006/anat/*_T1w.nii.gz' --input 'sourcedata/cneuromod.anat.gradcorrect/sub-03/ses-006/anat/*_T2w.nii.gz'  -- sourcedata/cneuromod.anat.gradcorrect ./ participant --steps cross-sectional --stages autorecon2 --refine_pial T2 --hires_mode enable --participant_label 03 --session_label 006 --skip_bids_validator --license_file code/freesurfer.license --n_cpus 4 
+datalad containers-run -m 'freesurfer_sub-03/ses-006' -n bids-freesurfer --output . --input 'sourcedata/cneuromod.anat.gradcorrect/sub-03/ses-006/anat/*_T1w.nii.gz' --input 'sourcedata/cneuromod.anat.gradcorrect/sub-03/ses-006/anat/*_T2w.nii.gz'  -- sourcedata/cneuromod.anat.gradcorrect ./ participant --steps cross-sectional --stages autorecon3 --refine_pial T2 --hires_mode enable --participant_label 03 --session_label 006 --skip_bids_validator --license_file code/freesurfer.license --n_cpus 4 
+freesurfer_exitcode=$?
+
+flock --verbose /lustre03/project/rrg-pbellec/ria-beluga/alias/cneuromod.anat.freesurfer_longitudinal/.datalad_lock datalad push -d ./ --to origin
